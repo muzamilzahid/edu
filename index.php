d12efb323690eaba6f7fc2eb82a53667c0662701
muzamilzahid <132767878+muzamilzahid@users.noreply.github.com>
2023-05-06 20:20:36 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login form</title>
+</head>
+<body>
+<center>
+<form method="post" action="login.php">
+<table border="2">
+<tr>
+<td> username:</td>
+<td><input type="text" name="uname" placeholder="enter username" required>
+</td></tr>
+<tr>
+<td>password:</td>
+<td><input type="password"
+ name="password" placeholder="enter password" required></td></tr>
+ <tr>
+ <td>
+ </td>
+ <td>
+ <input type="submit" name="submit" value="login">
+ 
+ </td>
+ </td>
+ </tr>
+
+</table>
+</form>
+</center>
+</body>
+</html>
